6dc368ae2b878f8c0276f80d0589bc55472de49c
Guilherme Strabello <gui.strabehonorio@outlook.com>
2023-07-18 21:28:58 -0300

    Att2

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,25 +1,95 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+// App.js
+import React, { useState } from 'react';
+import './App.scss';
 
+const App = () => {
+  const [selectedSection, setSelectedSection] = useState('dashboard');
+  const [searchQuery, setSearchQuery] = useState('');
 
-function App() {
+  const handleSectionClick = (section) => {
+    setSelectedSection(section);
+  };
+
+  const handleSearch = () => {
+    // Lógica de pesquisa aqui
+    console.log('Pesquisando por:', searchQuery);
+  };
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleLogout = () => {
+    // Lógica para deslogar o usuário aqui
+    console.log('Usuário deslogado!');
+  };
+
   return (
-    <div className="container">
-      <nav className="navbar app"> App Bar</nav>
-      <nav className="navbar board"> Board Bar</nav>
-      <div className="board-collumns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://trello.com/1/cards/5e6005496e20e75989cf9807/attachments/5e6005496e20e75989cf9898/previews/5e6005496e20e75989cf9894/download/Design.png"/>
-              Design and Research
-            </li>
-            <li>Second</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
+    <div className="app">
+      <aside className="sidebar">
+        <ul className="menu">
+        <li
+            id='logotipo'
+          >
+            CoShare
+          </li>
+          
+          <li
+            className={`menu-item ${selectedSection === 'dashboard' ? 'active' : ''}`}
+            onClick={() => handleSectionClick('dashboard')}
+          >
+            Dashboard
+          </li>
+          <li
+            className={`menu-item ${selectedSection === 'quadro' ? 'active' : ''}`}
+            onClick={() => handleSectionClick('quadro')}
+          >
+            Quadro
+          </li>
+          <li
+            className={`menu-item ${selectedSection === 'analise' ? 'active' : ''}`}
+            onClick={() => handleSectionClick('analise')}
+          >
+            Análise
+          </li>
+          <li
+            className={`menu-item ${selectedSection === 'configuracoes' ? 'active' : ''}`}
+            onClick={() => handleSectionClick('configuracoes')}
+          >
+            Configurações
+          </li>
+        </ul>
+        <button className="logout-button" onClick={handleLogout}>
+          Sair
+        </button>
+      </aside>
+      <div className="content">
+        <header className="header">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Pesquisar..."
+              value={searchQuery}
+              onChange={handleInputChange}
+            />
+            <button className="search-button" onClick={handleSearch}>
+            </button>
+          </div>
+        </header>
+        {/* Renderizar o conteúdo correspondente à seção selecionada */}
+        {selectedSection === 'dashboard' && <Dashboard />}
+        {selectedSection === 'quadro' && <Quadro />}
+        {selectedSection === 'analise' && <Analise />}
+        {selectedSection === 'configuracoes' && <Configuracoes />}
       </div>
     </div>
   );
-}
-//
+};
+
+const Dashboard = () => <h1>Dashboard</h1>;
+const Quadro = () => <h1>Quadro</h1>;
+const Analise = () => <h1>Análise</h1>;
+const Configuracoes = () => <h1>Configurações</h1>;
+
 export default App;
